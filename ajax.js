6c14b62443f53282c7be40bd83414ec1f1b2b8ea
Graham Moss <graham.moss@hotmail.com>
2017-02-16 15:50:40 -0500

    Complete steps 4,5

--- ajax.js
+++ ajax.js
@@ -12,14 +12,23 @@ $(document).ready(function () {
  $("#ajax-request-pp").on('click', function() {
 
    $.ajax({
-     url: 'http://first-ajax-api.herokuapp.com/ping',
+     url: 'http://first-ajax-api.herokuapp.com/pong',
      method: 'GET',
      data: 'text'
    }).done(function (responseData) {
      console.log(responseData);
-     $('section').append(responseData);
+     $('#step3456').append(responseData);
 
-   });
+   }).fail(function (jqXHR, textStatus, errorThrown) {
+     console.log(jqXHR, textStatus, errorThrown);
+     $('#step3456').append('Could not process request: Your computer sucks, you should probably invest in a new one.');
+
+   }).always(function() {
+     console.log('Hey the request finished!');
+
+
+   })
+
  });
 
 
