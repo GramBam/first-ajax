d6cc024a6204cb12eb84967c8d94956ec7dfce77
Graham Moss <graham.moss@hotmail.com>
2017-02-16 15:32:44 -0500

    Complete steps 1,2,3

--- ajax.js
+++ ajax.js
@@ -1,5 +1,29 @@
 $(document).ready(function () {
 
-  /* Your code goes here */
+  $("#ajax-request").on('click', function() {
+
+  $.ajax({
+  url: 'http://first-ajax-api.herokuapp.com/',
+  method: 'GET',
+  data: 'text'
+  });
+ });
+
+ $("#ajax-request-pp").on('click', function() {
+
+   $.ajax({
+     url: 'http://first-ajax-api.herokuapp.com/ping',
+     method: 'GET',
+     data: 'text'
+   }).done(function (responseData) {
+     console.log(responseData);
+     $('section').append(responseData);
+
+   });
+ });
+
+
+
+
 
 });
